25fb32f403f689bfd60bc4c65640a53e38a647de
stanleychukwu17 <stanleychukwu17@yahoo.com>
2021-10-10 07:54:59 +0100

    changes

--- components/partials/HeaderComp.tsx
+++ components/partials/HeaderComp.tsx
@@ -6,8 +6,32 @@ type HeaderPropsType = {
 
 export default function HeaderComp(props: HeaderPropsType) {
     return (
-        <div className={styles.mainCover}> 
-            the header is here
-        </div>
+        <>
+            <div className={styles.mainCover}>
+                <div className={styles.firNav}>
+                    <div className="">Hi <a href="jonah.com">Sign in</a> or <a href="mumu.com">Register</a></div>
+                    <div className="">Daily deals!</div>
+                    <div className="">Help &amp; Contact</div>
+                </div>
+                <div className={styles.secondNaV}>
+                    <div className="">Ships to</div>
+                    <div className="">Sell</div>
+                    <div className="">Watchlist</div>
+                    <div className="">My eBay</div>
+                </div>
+            </div>
+            <div className={styles.benDwnSec}>
+                <div className={styles.benLogo}><img src="/logo/ebay.png" alt="" /></div>
+                <div className={styles.shopByCat}>Shop by category</div>
+                <div className={styles.inputSrch}>
+                    <input type="text" />
+                    <select id="">
+                        <option value="">All categories</option>
+                    </select>
+                </div>
+                <div className={styles.inputBtn}><button>Search</button></div>
+                <div className={styles.inpAdvncd}>Advanced</div>
+            </div>
+        </>
     )
 }
